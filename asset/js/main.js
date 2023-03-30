d62a3644dce4de906b6fd82a7478dfc2c3013ecb
Federico Airoldi <airoldibusiness@gmail.com>
2023-03-31 00:10:38 +0200

    FASE 4 - imposto struttura per Even odd

--- asset/js/main.js
+++ asset/js/main.js
@@ -84,3 +84,12 @@ Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzion
 Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto. */
+
+
+/*
+
+STEP:
+
+1. Sistemo la struttura html
+
+*/
